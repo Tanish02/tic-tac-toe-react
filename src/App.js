0d5cbaad6a5875a720302a721a27424bd1d886a1
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-11-03 01:50:11 +0530

    WIP for calculatewinner

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ export default function Board() {
 
 
 function calcualtewinner(squares) {
-  const line = [
+  const lines = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -72,7 +72,13 @@ function calcualtewinner(squares) {
     [2, 4, 6]
   ];
 
-
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 
