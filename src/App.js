e1f6fd70fd9c580b6b4352cd4ecc6edaab5c36bf
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-11-03 01:35:29 +0530

    WIP for calculateWinner

--- src/App.js
+++ src/App.js
@@ -24,6 +24,9 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -54,6 +57,22 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+
+function calcualtewinner(squares) {
+  const line = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+
 }
 
 
